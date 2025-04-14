acb9ef62cd40c593367009ae2eba4713f9ad8aa9
Alon <alon@manukai.ch>
2025-04-14 12:17:12 +0200

    progress on lesson 4

--- 04/exercise/src/script.js
+++ 04/exercise/src/script.js
@@ -12,7 +12,16 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.set(0.7, -0.6, 1);
+mesh.scale.set(2, 0.5, 0.5);
+mesh.rotation.reorder('YXZ');
+mesh.rotation.y = Math.PI / 4;
+mesh.rotation.x = Math.PI / 4;
 scene.add(mesh)
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -28,6 +37,7 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+camera.lookAt(mesh.position)
 
 /**
  * Renderer
