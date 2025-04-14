96d463c1ff1ed6733f97da01d80a64f8b7d08336
Alon <alon@manukai.ch>
2025-04-14 08:54:28 +0200

    finish lesson 03

--- 03/exercise/script.js
+++ 03/exercise/script.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
 // Scene
 const scene = new THREE.Scene();
 
@@ -15,3 +18,9 @@ const sizes = { width: 800, height: 600 };
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 scene.add(camera);
+camera.position.z = 3;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
